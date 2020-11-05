6931877e0ba9e65e22c6abf6afa430635c487987
Yusuke Ota <yu.ota.1993@gmail.com>
2020-11-05 09:05:27 +0000

    リスト機能追加途中

---
+++ app/Http/Controllers/ListingsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Listing;
+use Auth;
+use Validator;
+
+use Illuminate\Http\Request;
+
+class ListingsController extends Controller
+{
+    //コンストラクタ
+    public function __construct()
+    {
+        //ログインしていなかったらログインページに遷移する
+        $this->middleware('auth');
+    }
+    
+    public function index()
+    {
+        $listings = Listing::where('user_id', Auth::user()->id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+        
+        //テンプレート  「listing/index.blade.php」を表示する  
+        return view('listing/index', ['listings' => $listings]);
+    }
+    
+    public function new()
+    {
+        //テンプレート「listing/new.blade.php」を表示する
+        return view('listing/new');
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,30 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
+//リスト一覧画面
+Route::get('/', 'ListingsController@index');
+
+//リスト新規画面
+Route::get('/new', 'ListingsController@new')->name('new');
+
+//リスト新規処理
+Route::post('/listings', 'ListingsController@store');
+
+//リスト更新画面
+Route::get('/listings_edit/{listing_id}', 'ListingsController@edit');
+
+//リスト更新処理
+Route::post('/listings/edit', 'ListingsController@update');
+
+//リスト削除処理
+Route::get('/listings_delete/{listing_id}', 'ListingsController@delete' );
+
+//ログイン認証
 Auth::routes();
 
+//ホーム画面
 Route::get('/home', 'HomeController@index')->name('home');
