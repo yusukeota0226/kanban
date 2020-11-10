5475674c1327b2e5cd2848a9fb24d61a95bd8729
Yusuke Ota <yu.ota.1993@gmail.com>
2020-11-10 00:43:31 +0000

    カード新規登録処理追加

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -22,4 +22,27 @@ class CardsController extends Controller
         //テンプレート「card/new.blade.php」を表示する
         return view('card/new', ['listing_id' => $listing_id]);
     }
+    
+    public function store(Request $request)
+    {
+        //バリデーションチェック
+        $validator = Validator::make($request->all(), ['card_title' => 'required|max:255', 'card_memo' => 'required|max:255',]);
+        
+        //バリデーションチェックがエラーの場合
+        if($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator->errors())->withInput();
+        }
+        
+        //Cardの登録処理
+        $cards = new Card;
+        $cards->title = $request->card_title;
+        $cards->listing_id = $request->listing_id;
+        $cards->memo = $request->card_memo;
+        
+        $cards->save();
+        
+        //ルートにリダイレクト
+        return redirect('/');
+    }
 }
